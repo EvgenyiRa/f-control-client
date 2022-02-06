d833e595e2328c2b9c57ceb582b65473cd2508f0
Ra <tryampampam@mail.ru>
2022-02-06 21:36:01 +0300

    Доработка клиента

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 const { performance } = require('perf_hooks'),
       rp = require('request-promise'),
-      { exec } = require("child_process");
+      util = require('util'),
+      execSync = require('child_process').execSync;
 
-console.log("Start f-control");
-
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+async function run() {
+  try {
+    console.log("Start f-control");
+    const stdout = execSync('ps -eF');
+    let process=stdout.toString().split(String.fromCharCode(10)),
+        processAll=[];
+    const oneStrWork=(process_i)=>{
+      let tekStrAll=process_i,
+          oneStr=[],
+          tekStr=tekStrAll[0];
+      //console.log(tekStr);
+      //processAll.push(.split(' '))
+      for (var j = 1; j < tekStrAll.length; j++) {
+        if (tekStrAll[j]===' ') {
+          if (tekStr!==' ') {
+              if (tekStrAll[j+1]!=='-') {
+                oneStr.push(tekStr);
+                tekStr=' ';
+              }
+              else {
+                  tekStr+=tekStrAll[j];
+              }
+          }
+          else {
+              tekStr=' ';
+          }
+        }
+        else {
+          if (tekStr!==' ') {
+              tekStr+=tekStrAll[j];
+          }
+          else {
+              tekStr=tekStrAll[j];
+          }
+        }
+      }
+      oneStr.push(tekStr);
+      return oneStr;
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+    const processNameObj=oneStrWork(process[0]),
+          processNameObjKey={};
+    processNameObj.push('PNAME');
+    for (var i = 0; i < processNameObj.length; i++) {
+      processNameObjKey[processNameObj[i]]=i;
     }
-    console.log(`stdout: ${stdout}`);
-});
+    console.log(processNameObjKey);
+    for (var i = 1; i < process.length; i++) {
+      const oneStr=oneStrWork(process[i]);
+      if (!isNaN(oneStr[processNameObjKey['PID']])) {
+        let procName=oneStr[processNameObjKey['CMD']];
+        try {
+          const stdoutPN = execSync("ps -p "+oneStr[processNameObjKey['PID']]+" -o comm=");
+          //console.log(stdoutPN.toString());
+          procName=stdoutPN.toString();
+          if (procName[procName.length-1]===String.fromCharCode(10)) {
+            procName=procName.slice(0, -1);
+            console.log(procName);
+          }
+        }
+        catch (e) {
+          //console.log(e);
+          //console.log(oneStr);
+        }
+        oneStr.push(procName);
+      }
+      else {
+        console.log(process[i]);
+      }
+      processAll.push(oneStr);
+      //break;
+    }
+    //console.log(processAll);
+  } catch (e) {
+    console.error(e); // should contain code (exit code) and signal (that caused the termination).
+  }
+}
+run();
