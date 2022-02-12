d39b1aa7d15bcd14ad25eb180cca30ab045fd2b0
Ra <tryampampam@mail.ru>
2022-02-12 14:39:36 +0300

    Доработка клиента

--- configs/configs.js
+++ configs/configs.js
@@ -3,6 +3,7 @@ const configs={
   webServerProtocol: 'http',
   webSocketProtocol:'ws',
   repUserId: 1,
+  keyForWebServer:'ca2424d4114217b607ccefcfd6d1596b0836c8cf5724b3146ab04e3e35e679e7',
   countMSsave:3000,
   counMSupd:1000,
   test:true,
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DISPLAY=:0
 cd /var/www/nodejs/f-control-client
-exec sudo -u ra /bin/sh - << eof
+exec sudo -u $(whoami) /bin/sh - << eof
 node .
