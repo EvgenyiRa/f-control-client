0883f6a851d15aaf538b6ede409835448258b150
Ra <tryampampam@mail.ru>
2022-04-21 15:25:20 +0300

    Доработка клиента

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ function killWaste {
       while [[ ${#usrAcount[*]}>1 ]]
         do
           #в живых должен остаться только один!
-          IFS=' ' read -r -a usrA <<< "${usrAcount[0]}"
-          killall -w -u ${usrA[0]}
+          IFS=' ' read -r -a usrA <<< "${usrAcount[0]}";
+          killall -w -u ${usrA[0]};
+          #echo "killall -w -u ${usrA[0]};"
           usr=$(who -s)
           usrFull=$usr;
           IFS=$'\n'; usrAcount=($usr); unset IFS;
@@ -34,6 +35,7 @@ function killWaste {
 }
 killWaste;
 usrRun=$usr;
+nodePID=-777;
 function runNode {
   usrRun=$usr;
   echo "pre exex user: $usrRun"
@@ -41,7 +43,10 @@ function runNode {
   IFS=' ' read -r -a dispE <<< "${usrD[1]}"
   echo "DISPLAY=${dispE[0]}"
   export DISPLAY=:${dispE[0]}
-  sudo -u $usrRun -g root /bin/sh node .
+  sudo -u $usrRun -g root /bin/sh node . &
+  nodePID=$!;
+  echo "nodePID: $nodePID"
+  #echo "sudo -u $usrRun -g root /bin/sh node ."
 }
 runNode;
 #мониторим кол-во пользователей
@@ -51,6 +56,14 @@ while true ; do
   killWaste;
   if [ "$usr" != "$usrRun" ]; then
     runNode;
+  else
+    #проверяем наличие процесса
+    psRes=$(ps -p $nodePID);
+    IFS=$'\n'; psResCount=($psRes); unset IFS;
+    if [ ${#psResCount[*]} -eq 1 ]; then
+      #если упал, поднимаем
+      runNode;
+    fi
   fi
   sleep 10;
 done
