9a9e2b83d929aa10f5a9f6b99829aa1ed2c61ec6
Ra <tryampampam@mail.ru>
2022-04-27 17:45:30 +0300

    Доработка клиента

--- install.sh
+++ install.sh
@@ -6,15 +6,52 @@ osID=${osID/"ID="/""};
 echo "osID=$osID";
 case $osID in
   ubuntu)
-    echo "yes ubuntu!"
-    ;;
+    #echo "yes ubuntu!"
+    curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -;
+    sudo apt-get install -y nodejs;
+    sudo apt install git;
+  ;;
+  debian)
+    curl -fsSL https://deb.nodesource.com/setup_16.x | bash -;
+    apt-get install -y nodejs;
+    apt install git;
+  ;;
   *)
     #
-    ;;
+  ;;
 esac
 
-#скачиваем проект
+#создаем папку проекта
+dirProject="/opt/f-control";
+fccName="f-control-client";
+fccChName="f-control-client-ch";
+gitUrl="https://github.com/EvgenyiRa/";
+mkdir -p $dirProject;
+#скачиваем проекты
+git clone "$gitUrl$fccName.git" $dirProject;
+git clone "$gitUrl$fccChName.git" $dirProject;
 
+#получаем текущего пользователя
+usr=$(who -s);
+countUsr=$(echo "$usr"|wc -l);
+if [ $countUsr -gt 1 ]; then
+  echo "$usr" | while read usr
+  do
+    #echo "usr=$usr"
+    IFS=' ' read -r -a usrA <<< "$usr";
+    if [[ "${usrA[1]}" == *":"* ]]; then
+      usrFull=$usr;
+      break;
+    fi
+  done;
+fi
+IFS=' ' read -r -a usrA <<< "$usr"
+usr=${usrA[0]}
 #устанавливаем группу root на папку и добавляем права группе
+chown -R $usr:root "$dirProject/$fccName/*";
+chmod 771 "$dirProject/$fccName/*"
+chmod ugo+x "$dirProject/$fccName/run.sh"
 
 #создаем задание для демона
+serviceVal=$(sed 's/\{currentPath\}/'"$dirProject/$fccName"'/'  -i   "$dirProject/$fccName/$fccName.service.template");
+echo "$serviceVal" > "/etc/systemd/system/$fccName.service";
