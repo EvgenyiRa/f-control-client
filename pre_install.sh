24466544787cd61df98d8159fac2d9153ad4bfe8
Ra <tryampampam@mail.ru>
2022-05-15 20:06:12 +0300

    Доработка клиента

---
+++ pre_install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#установка node.js, git
+#получаем тип ОС
+osID=$(grep ^ID= /etc/os-release);
+osID=${osID/"ID="/""};
+echo "osID=$osID";
+case $osID in
+  ubuntu)
+    #echo "yes ubuntu!"
+    #удаляем firefox
+    apt-get purge firefox
+    #устанавливаем chrome
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+    #отключаем и перезапускаем wayland
+    daemonConfVal="";
+    prSearch=false;
+    dirDaemonConf="/etc/gdm3/daemon.conf";
+    while IFS= read -r line
+    do
+      if [[ "$line" == *"WaylandEnable"* ]]; then
+        line="WaylandEnable = false\n";
+        prSearch=true;
+      fi
+      daemonConfVal="${daemonConfVal}$line";
+    done < "$dirDaemonConf"
+    if [ "$prSearch" = false ] ; then
+      daemonConfVal="${daemonConfVal}WaylandEnable = false\n";
+    fi
+    echo "$daemonConfVal" > "$dirDaemonConf";
+    if [ "$XDG_SESSION_TYPE" != "x11" ]; then
+        service gdm3 restart;
+    fi
+  ;;
+  debian)
+
+  ;;
+  *)
+    #
+  ;;
+esac
