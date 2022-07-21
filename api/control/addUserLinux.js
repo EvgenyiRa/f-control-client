274626303cc43eddbce74f4fb2e2254431023898
e.rassadnikov <tryampampam@mail.ru>
2022-07-21 19:46:12 +0300

    Доработка клиента

--- api/control/addUserLinux.js
+++ api/control/addUserLinux.js
@@ -1,10 +1,11 @@
 'use strict';
 const execSync = require('child_process').execSync;
 
-module.exports = async (login,pwd) => {
+module.exports = async (pwdAdm,login,pwd) => {
   let result=true;
-  try {    
-    execSync('exec  '+nodeD+' '+dirS);
+  try {
+    //echo "pwd|sudo -S useradd test3 -p 111111 -m -U -s /bin/bash
+    execSync(`useradd ${login} -p ${pwd} -m -U -s /bin/bash`);
   } catch (err) {
     console.log(err);
     result=false;
