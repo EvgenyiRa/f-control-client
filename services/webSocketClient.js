a6f19f5126bf2e1306f8b9ccfa6dd4d82d3ed60a
e.rassadnikov <tryampampam@mail.ru>
2022-09-11 19:27:12 +0300

    Доработка клиента

--- services/webSocketClient.js
+++ services/webSocketClient.js
@@ -1,85 +1,162 @@
 // Создаётся экземпляр клиента
+'use strict';
 const configs=require('../configs/configs.js'),
-      WebSocketClient = require('websocket').client,
-      fs = require('fs');
+      W3CWebSocket = require('websocket').w3cwebsocket,
+      { performance } = require('perf_hooks');
 
-console.log("Подключение к серверу "+configs.webServer+" по WebSocket");
-const connectionObj={};
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+const api={},
+      apiStr={};
 let wsClient;
+const init=(data)=> {
+  return new Promise((resolve) => {
+    const wsStat={
+      connect:false,
+      auth:false,
+      keyAuth:undefined
+    };
+    const resolveObj={};
 
-const init=(data)=>{
-  // Вешаем на него обработчик события подключения к серверу
-  data.wsStat.connect=false;
-  data.wsStat.auth=false;
-  data.wsStat.dataUpdate=false;
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  wsClient = new WebSocketClient();
-  wsClient.on('connect', wsHandler);
-  function wsHandler(connection) {
-    console.log('WebSocket Client Connected');
-    data.wsStat.connect=true;
-    data.wsStat.connection=connection;
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-        data.wsStat.connect=false;
-        data.wsStat.auth=false;
-        data.wsStat.dataUpdate=false;
-        wsClient.abort();
-    });
-    connection.on('close', function() {
-        console.log('Connection close');
-        data.wsStat.connect=false;
-        data.wsStat.auth=false;
-        data.wsStat.dataUpdate=false;
-        wsClient.abort();
-    });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            //console.log("Received: '" + message.utf8Data + "'");
-            try {
-              const dataP=JSON.parse(message.utf8Data);
-              if (dataP.type==='auth') {
-                  // посылаем сообщение серверу
-                  const request={
-                    type:'auth',
-                    message:"it's my, open!",
-                    webServerLogin:data.webServerLogin,
-                    login:data.login,
-                    webServerPwd:data.webServerPwd
-                  };
-                  connection.sendUTF(JSON.stringify(request));
+    console.log("Подключение к серверу "+configs.webServer+" по WebSocket");
+    wsClient = new W3CWebSocket(configs.webSocketServer+'/api', 'echo-protocol');
+
+    wsClient.onopen = function() {
+        console.log("Соединение установлено");
+        wsStat.connect=true;
+    };
+
+    wsClient.onmessage = (event) => {
+      try {
+        const dataP=JSON.parse(event.data);
+        if (dataP.type==='auth') {
+            // посылаем сообщение серверу
+            const request={
+              type:'authClient',
+              message:"it's my, open!",
+              webServerLogin:data.webServerLogin,
+              webServerPwd:data.webServerPwd,
+              login:data.login
+            };
+            wsSend(JSON.stringify(request));
+        }
+        else if (dataP.type==='authRes') {
+          wsStat.auth=dataP.data.auth;
+          console.log('Auth result',dataP.data);
+          if (wsStat.auth) {
+            wsStat.keyAuth=dataP.data.key;
+            wsStat.user=dataP.data.user;
+            for (const method of dataP.data.methods) {
+              const path=method.split('.');
+              let i=0;
+              const setApiTree=(apiIn,i)=>{
+                  if (i===(path.length-1)) {
+                      apiIn[path[i]] = (...args) => new Promise((resolve2) => {
+                        const getMethod=()=>{
+                          const id=performance.now();
+                          resolveObj[id]=resolve2;
+                          wsSend(JSON.stringify({
+                            type:"method",
+                            method:method,
+                            args:args,
+                            id:id,
+                            keyAuth:wsStat.keyAuth
+                          }));
+                        }
+                        if ((wsStat.auth) & (wsStat.connect)) {
+                          if(wsClient.readyState!== wsClient.OPEN){
+                              setTimeout(function (){
+                                  if(wsClient.readyState!== wsClient.OPEN){
+                                      wsStat.connect=false;
+                                      wsClient.close();
+                                      init(wsStat.keyAuth,true).then((resWsCon) => {
+                                        if (resWsCon) {
+                                            getMethod();
+                                        }
+                                        else {
+                                            resolve2(false);
+                                        }
+                                      })
+                                  }
+                                  else {
+                                      getMethod();
+                                  }
+                              },3000);
+                          } else {
+                              getMethod();
+                          }
+                        }
+                        else if (!wsStat.connect) {
+                            init(wsStat.keyAuth,true).then((resWsCon) => {
+                              if (resWsCon) {
+                                  getMethod();
+                              }
+                              else {
+                                  resolve2(false);
+                              }
+                            })
+                        }
+                        else {
+                            resolve2(false);
+                        }
+                      });
+                      apiStr[method]=apiIn[path[i]];
+                  }
+                  else {
+                      if (!!!apiIn[path[i]]) {
+                        apiIn[path[i]]={};
+                      }
+                      setApiTree(apiIn[path[i]],++i)
+                  }
               }
-              else if (dataP.type==='authRes') {
-                data.wsStat.auth=dataP.data.auth;
-                if (data.wsStat.auth) {
-                    data.lims=dataP.data.lims;
-                    fs.writeFileSync("./data/lims/"+data.login+".json", JSON.stringify(data.lims));
-                }
-              }
-              else if (dataP.type==='dataUpdateRes') {
-                console.log(dataP);
-              }
-            } catch (err) {
-              console.log('try wsServer err msg: ', err);
+              setApiTree(api,i);
+            };
+            //console.log('apiStr',apiStr);
+            resolve(wsStat);
+          }
+          else {
+            resolve(wsStat);
+          }
+        }
+        else if (dataP.type==='method') {
+            if (!!resolveObj[dataP.id]) {
+              resolveObj[dataP.id](dataP.data);
+              delete resolveObj[dataP.id];
             }
         }
-    });
-  }
-  // Подключаемся к нужному ресурсу
-  wsClient.connect(configs.webSocketServer,{ rejectUnauthorized: false });
-  //ошибка подключения
-  wsClient.on('connectFailed', function(error) {
-      console.log('connectFailed ' + error.toString());
-      data.wsStat.connect=false;
-      data.wsStat.auth=false;
-      data.wsStat.dataUpdate=false;
-      wsClient.abort();
+      } catch (err) {
+        console.log('try wsServer err msg: ', err);
+      }
+    }
+
+    wsClient.onclose = function(event) {
+        if (event.wasClean) {
+            console.log('Соединение закрыто чисто');
+        } else {
+            console.log('Обрыв соединения'); // например, "убит" процесс сервера
+        }
+        console.log('Код: ' + event.code + ' причина: ' + event.reason);
+        wsStat.connect=false;
+        resolve(wsStat);
+    };
+
+    wsClient.onerror = function(error) {
+        console.log("Ошибка " + error.message);
+        wsStat.connect=false;
+        resolve(wsStat);
+    };
+
+    const wsSend = (data)=> {
+      // readyState - true, если есть подключение
+      wsClient.send(data);
+    }
   });
 }
-module.exports.init = init;
 
-const wsAbort=()=>{
-    wsClient.abort();
+const wsClose=()=>{
+    if (!!wsClient) {
+      wsClient.close();
+    }
 }
-module.exports.wsAbort=wsAbort;
+
+export {init,wsClose,api,apiStr};
