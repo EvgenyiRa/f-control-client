01149267798d9b0a8f627c58cc0725c27321e107
e.rassadnikov <tryampampam@mail.ru>
2022-10-09 20:22:19 +0300

    Доработка клиента

--- api/control/getLims.js
+++ api/control/getLims.js
@@ -5,11 +5,13 @@ const path = require('path'),
 module.exports = async () => {
   const pathRoot=path.dirname(path.dirname(__dirname)),
         dir=path.join('data','lims'),
-        files=fs.readdirSync(dir),
         result={};
-  for (var i = 0; i < files.length; i++) {
-    const dirF=path.join(dir,files[i]);
-    result[files[i].split('.json')[0]]=JSON.parse(fs.readFileSync(dirF, 'utf8'));
+  if (fs.existsSync(dir)) {     
+    const files=fs.readdirSync(dir);
+    for (var i = 0; i < files.length; i++) {
+      const dirF=path.join(dir,files[i]);
+      result[files[i].split('.json')[0]]=JSON.parse(fs.readFileSync(dirF, 'utf8'));
+    }
   }
   return result;
 };
